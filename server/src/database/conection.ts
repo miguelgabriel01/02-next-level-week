0359882e70ac9ecd458abf6edcccb815b1b68661
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-08-07 21:20:28 -0500

    Criação de arquivo de configuração com o Bano de dados

---
+++ server/src/database/conection.ts
@@ -0,0 +1,13 @@
+//arquivo de conexão com o  banco de dados
+import knex from 'knex';//importação do knex
+import path from 'path';//O pathmódulo fornece utilitários para trabalhar com caminhos de arquivos e diretórios.
+
+//salvamos em uma constante a função
+const db = knex({
+  client: 'sqlite3',//banco usado
+  connection:{
+    filename: path.resolve(__dirname, 'database.sqlite')
+  },
+  useNullAsDefault: true,//essa configuração é responsavel por fazer com que o banco adicione NULL aos valores não identificados
+  
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,43 @@
-console.log("Olá mundo")
+import express from 'express';//importação do express
+
+const app = express();//salva em uma constante a função express
+
+app.use(express.json());//por padrão, o Express não entendi JSON. por isso, esta configuração deve ser feita 
+
+//--------------------------------------------------------------------------------------------
+//Metodos HTTP:
+
+//GET: Buscar ou listar um informação
+//POST: Criar uma nova informação
+//PUT: Atualizar uma informação já existente
+//DELETE: Deletar uma informação
+
+//Corpo ( Requeste Body ): Dados para criação ou atualização de um registro
+//Route Params: Identificar qual recurso eu quero atualizar ou apagar
+//Query Params: Paginação, filtros,ordenação
+
+//OBS: O navegador, por padrão, sempre acessa uma rota pelo metodo GET
+
+//--------------------------------------------------------------------------------------------
+
+app.get('/users', ( req, res ) => {
+ return res.json({ Mesage: "Olá mundo"});
+})
+
+//este metodo faz com que nossa aplicação "escute" um endreço HTTP( Ouvir requisições HTTP )
+const PORT = 3333;//porta que a aplicação ira rodar
+app.listen(PORT,() => {
+
+  console.log(" ")
+  console.log("--------------------------------")
+  console.log("Servidor iniciado na porta 3333");
+  console.log("Para derrubar servidor: CTRL + C");
+  console.log("github.com/miguelgabriel01")
+  console.log("mgbs@discente.ifpe.edu.br")
+  console.log("  ")
+
+  console.log("--------------------------------")
+
+  console.log(" ")
+  console.log("CRISTO VEM!!!!!!!!!")
+})
